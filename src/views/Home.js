cb8934d1bd94e02d57f07b26fdb887a5d3a29b91
Italo Guajardo <italog.penaloza@gmail.com>
2021-09-14 13:05:29 -0300

    change home page

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,9 +1,34 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export const Home = () => {
     return (
         <div>
-            <h1>Home</h1>
-        </div>
+            <h1>Star Wars Databank</h1>
+            <hr />
+            <div className="row">
+                <div className="col-12 col-md-6">
+                    <ul className="list-group">
+                        <li className="list-group-item">
+                            <Link className="nav-link" to="/characters">
+                                Characters
+                            </Link>
+                        </li>
+                        <li className="list-group-item">
+                            <Link className="nav-link" to="/planets">
+                                Planets
+                            </Link>
+                        </li>
+                        <li className="list-group-item">
+                            <Link className="nav-link" to="/starships">
+                                Starships
+                            </Link>
+                        </li>
+
+                    </ul>
+
+                </div>
+            </div>
+        </div >
     )
 }
